a237bf6e90694610c3b2ca55f80ccccdecd33c99
Donovan Cotter <DetroitLabs@DonovanDLabsMacBookPro.local>
2016-06-27 15:19:13 -0400

    set up storyboard with outlets and IBActions for the login and signup viewControllers

---
+++ Wire/Wire/LoginViewController.m
@@ -0,0 +1,35 @@
+//
+//  LoginViewController.m
+//  Wire
+//
+//  Created by DetroitLabs on 6/27/16.
+//  Copyright © 2016 Srinivas Bodhanampati. All rights reserved.
+//
+
+#import "LoginViewController.h"
+
+@interface LoginViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *emailTF;
+@property (weak, nonatomic) IBOutlet UITextField *passwordTF;
+
+@end
+
+@implementation LoginViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)loginButtonPressed:(id)sender {
+    
+}
+
+
+
+@end
---
+++ Wire/Wire/SignUpViewController.h
@@ -0,0 +1,13 @@
+//
+//  SignUpViewController.h
+//  Wire
+//
+//  Created by DetroitLabs on 6/27/16.
+//  Copyright © 2016 Srinivas Bodhanampati. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SignUpViewController : UIViewController
+
+@end
