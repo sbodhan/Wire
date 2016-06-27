9cf45c84ec800a18f640010dd6cd4df1e3defc88
Srinivas Bodhanampati <sbodhan@Srinivass-MacBook-Pro.local>
2016-06-27 10:59:00 -0400

    Initial Version

---
+++ Wire/Wire/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Wire
+//
+//  Created by Srinivas Bodhanampati on 6/27/16.
+//  Copyright © 2016 Srinivas Bodhanampati. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Wire/Wire/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Wire
+//
+//  Created by Srinivas Bodhanampati on 6/27/16.
+//  Copyright © 2016 Srinivas Bodhanampati. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
