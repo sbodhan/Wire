1ed82fd791660f8df753d136ae540d318914ba55
Donovan Cotter <DetroitLabs@DonovanDLabsMacBookPro.local>
2016-06-27 13:32:35 -0400

    added IBActions for buttons and outlets for the textfields on login and sign up screens

--- Wire/Wire/SignUpViewController.m
+++ Wire/Wire/SignUpViewController.m
@@ -9,6 +9,10 @@
 #import "SignUpViewController.h"
 
 @interface SignUpViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *emailTF;
+@property (weak, nonatomic) IBOutlet UITextField *passwordTF;
+@property (weak, nonatomic) IBOutlet UITextField *repeatPasswordTF;
+@property (weak, nonatomic) IBOutlet UITextField *usernameTF;
 
 @end
 
@@ -24,14 +28,10 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (IBAction)signUpButtonPressed:(id)sender {
+    
+    
 }
-*/
+
 
 @end
