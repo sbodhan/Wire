c1ca79e1ff58277fa8515b6831eed78d07adf7ef
Donovan Cotter <DetroitLabs@DonovanDLabsMacBookPro.local>
2016-06-29 11:49:37 -0400

    JSQMessagesVC is working but is hardcoded and not pulling down the messages from Firebase

--- Wire/Wire/ChatViewController.m
+++ Wire/Wire/ChatViewController.m
@@ -7,16 +7,28 @@
 //
 
 #import "ChatViewController.h"
+#import "JSQMessage.h"
+#import "JSQMessagesBubbleImage.h"
+#import "JSQMessagesAvatarImage.h"
+#import "JSQMessagesBubbleImageFactory.h"
 @import FirebaseDatabase;
 
 @interface ChatViewController ()
 @property (nonatomic, strong) NSMutableArray *messages;
+@property (nonatomic, strong) JSQMessagesBubbleImage *outgoingBubbleImage;
+@property (nonatomic, strong) JSQMessagesBubbleImage *incomingBubbleImage;
+@property (nonatomic, strong) NSMutableArray *avatars;
+
 @end
 
 @implementation ChatViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self JSQMessageBubbleSetup];
+    _messages = [[NSMutableArray alloc]init];
+    _avatars = [[NSMutableArray alloc]init];
+
     
     //THESE ARE ONLY FOR TESTING SO APP WON'T CRASH!
     self.senderId = @"3252646";
@@ -34,11 +46,52 @@
     NSString *timestamp = [NSString stringWithFormat:@"%@", date];
     NSDictionary *message = @{@"text": text, @"senderId": senderId, @"senderName": senderDisplayName, @"timestamp":timestamp};
     [self sendMessageToFirebase:message];
+    
+    //ONLY FOR TESTING PURPOSES: *****************************************
+    JSQMessage *mssg = [[JSQMessage alloc]initWithSenderId:senderId senderDisplayName:senderDisplayName date:date text:text];
+    [_messages addObject:mssg];
+    [self.collectionView reloadData];
+    //ONLY FOR TESTING PURPOSES: *****************************************
+    
 }
 
-//Number of items in section
+//Number of items in section **How many items in each section - in our case it will be however many messages we have** - REQUIRED
 -(NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
     return [_messages count];
+}
+
+//Message Data for item at indexPath **Data source for the messages** - REQUIRED
+-(id<JSQMessageData>)collectionView:(JSQMessagesCollectionView *)collectionView messageDataForItemAtIndexPath:(NSIndexPath *)indexPath {
+
+    return [_messages objectAtIndex:indexPath.row];
+}
+
+//MessageBubbleImageData for item at indexPath **this is for the bubble image behind each text** - REQUIRED
+-(id<JSQMessageBubbleImageDataSource>)collectionView:(JSQMessagesCollectionView *)collectionView messageBubbleImageDataForItemAtIndexPath:(NSIndexPath *)indexPath {
+    JSQMessage *message = _messages[indexPath.row];
+    
+    if ([message.senderId isEqualToString:self.senderId]) {
+        return self.outgoingBubbleImage;
+    } else {
+        return self.incomingBubbleImage;
+    }
+}
+
+//AvatarImageData for item at indexPath **this is the avatarImage that needs to be supplied for each text** - REQUIRED
+-(id<JSQMessageAvatarImageDataSource>)collectionView:(JSQMessagesCollectionView *)collectionView avatarImageDataForItemAtIndexPath:(NSIndexPath *)indexPath {
+    
+    UIImage *avatarImage = [UIImage imageNamed:@"snapchat"];
+    
+    JSQMessagesAvatarImage *avatar = [JSQMessagesAvatarImage avatarWithImage:avatarImage];
+    [_avatars addObject:avatar];
+    
+    return avatar;
+}
+
+-(void)JSQMessageBubbleSetup {
+    JSQMessagesBubbleImageFactory *bubbleFactory = [[JSQMessagesBubbleImageFactory alloc]init];
+    _outgoingBubbleImage = [bubbleFactory outgoingMessagesBubbleImageWithColor:[UIColor blueColor]];
+    _incomingBubbleImage = [bubbleFactory incomingMessagesBubbleImageWithColor:[UIColor grayColor]];
 }
 
 #pragma mark Firebase Methods
@@ -46,6 +99,7 @@
 -(void)sendMessageToFirebase:(NSDictionary *)message {
     FIRDatabaseReference *messagesRef = [[[[FIRDatabase database]reference]child:@"messages"]childByAutoId];
     [messagesRef setValue:message];
+    
 }
 
 @end
