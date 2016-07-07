eda010326c8aecc60d0179c85278c675591c9a57
dcotter1208 <donovan.cotter@yahoo.com>
2016-07-07 09:18:27 -0400

    reduced the size of the profile image to speed up download speed

--- Wire/Wire/ChatProfileVC.m
+++ Wire/Wire/ChatProfileVC.m
@@ -14,13 +14,15 @@
 @import FirebaseStorage;
 
 @interface ChatProfileVC ()<UINavigationControllerDelegate, UIImagePickerControllerDelegate>
+
+#pragma mark Properties
 @property (strong, nonatomic) UIImagePickerController *imagePicker;
-//FIRStorageReference represents a reference to a Google Cloud Storage object.
 @property (strong, nonatomic) FIRStorageReference *firebaseStorageRef;
 @property (strong, nonatomic) NSString *currentUserProfileKey;
-//An instance of FIRStorage will initialize with the default FIRApp
 @property (strong, nonatomic) FIRStorage *firebaseStorage;
 @property(strong, nonatomic) UserProfile *currentUser;
+
+#pragma mark IBOutlets
 @property (weak, nonatomic) IBOutlet UILabel *usernameLabel;
 @property (weak, nonatomic) IBOutlet UIImageView *currentUserProfilePhoto;
 
@@ -28,9 +30,13 @@
 
 @implementation ChatProfileVC
 
+#pragma mark Lifecycle Methods
 - (void)viewDidLoad {
+    //initializes Firebase Storage and creates reference to it.
     [self firebaseSetUp];
+    //Sets up a listener for changes in the user's profile such as the profilePhotoDownloadURL.
     [self listenForChangesInUserProfile];
+    //Initially gets the UserProfile associated with the currently logged in user.
     [self getCurrentUserProfileFromFirebase];
     [super viewDidLoad];
         
@@ -40,23 +46,54 @@
     [super didReceiveMemoryWarning];
 }
 
-- (IBAction)signOutBtnPressed:(id)sender {
-    NSError *error;
-    [[FIRAuth auth] signOut:&error];
-    if (!error) {
-
-    }
+#pragma mark Camera Methods.
+//Presents the iPhone's camera and is called when the profile photo is selected.
+-(void)presentCamera {
+    _imagePicker = [[UIImagePickerController alloc] init];
+    [_imagePicker setDelegate:self];
+    [_imagePicker setSourceType:UIImagePickerControllerSourceTypeCamera];
+    [self presentViewController:_imagePicker animated:true completion:nil];
 }
 
+/*
+ Occurrs when the camera finishes taking the photo.
+ It then uses the reduceImageSize func and uploadPhotoToFirebase func
+ to reduce the image's size and then send it to Firebase.
+ */
+-(void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info {
+    NSData *imageData = UIImageJPEGRepresentation([info objectForKey:@"UIImagePickerControllerOriginalImage"], 1);
+    UIImage *image = [UIImage imageWithData:imageData];
+    NSData *resizedImgData =  UIImageJPEGRepresentation([self reduceImageSize:image], .80);
+    [self uploadPhotoToFirebase:resizedImgData];
+    [self dismissViewControllerAnimated:true completion:nil];
+}
+
+//Accepts a UIImage and reduces the size of it.
+-(UIImage *)reduceImageSize:(UIImage *)image {
+    CGSize newSize = CGSizeMake(image.size.width/10, image.size.height/10);
+    UIGraphicsBeginImageContextWithOptions(newSize, false, 0.0);
+    [image drawInRect:CGRectMake(0, 0, newSize.width, newSize.height)];
+    UIImage *smallImage = UIGraphicsGetImageFromCurrentImageContext();
+    return smallImage;
+}
+
+#pragma mark Firebase Methods
+
+//initializes Firebase Storage and creates reference to it.
+-(void)firebaseSetUp {
+    _firebaseStorage = [FIRStorage storage];
+    _firebaseStorageRef = [_firebaseStorage referenceForURL:@"gs://wire-e0cde.appspot.com"];
+}
+
+
+//Gets the current user's UserProfile from Firebase.
 -(void)getCurrentUserProfileFromFirebase {
     FIRDatabaseReference *UserProfileRef = [[[FIRDatabase database]reference]child:@"userprofile"];
     FIRDatabaseQuery *currentUserProfileQuery = [[UserProfileRef queryOrderedByChild:@"userId"] queryEqualToValue:[FIRAuth auth].currentUser.uid];
     [currentUserProfileQuery observeEventType:FIRDataEventTypeChildAdded withBlock:^(FIRDataSnapshot *snapshot) {
-        NSLog(@"SNAPSHOT %@", snapshot);
         _currentUserProfileKey = snapshot.key;
         _currentUser = [[UserProfile alloc]initUserProfileWithEmail:snapshot.value[@"email"] username:snapshot.value[@"username"] uid:snapshot.value[@"userId"]];
         _currentUser.profileImageDownloadURL = snapshot.value[@"profilePhotoDownloadURL"];
-        NSLog(@"DOWNLOADED: %@", snapshot.value[@"profilePhotoDownloadURL"]);
         _currentUser.profileImage = [UIImage imageWithData:[NSData dataWithContentsOfURL:[NSURL URLWithString:snapshot.value[@"profilePhotoDownloadURL"]]]];
         dispatch_async(dispatch_get_main_queue(), ^{
             [_currentUserProfilePhoto setImage:_currentUser.profileImage];
@@ -65,10 +102,16 @@
     }];
 }
 
+/*
+ Listens for changes to the current user's UserProfile.
+ This uses FIRDataEventTypeChildChange, which is similar to FIRDataEventTypeChildAdded
+ except that it occurrs when a child node's value is changed in some way and not
+ when a new child is added.
+ */
 -(void)listenForChangesInUserProfile {
     FIRDatabaseReference *UserProfileRef = [[[FIRDatabase database]reference]child:@"userprofile"];
     FIRDatabaseQuery *currentUserProfileChangedQuery = [[UserProfileRef queryOrderedByChild:@"userId"] queryEqualToValue:[FIRAuth auth].currentUser.uid];
-
+    
     [currentUserProfileChangedQuery observeEventType:FIRDataEventTypeChildChanged withBlock:^(FIRDataSnapshot *snapshot) {
         _currentUser = [[UserProfile alloc]initUserProfileWithEmail:snapshot.value[@"email"] username:snapshot.value[@"username"] uid:snapshot.value[@"userId"]];
         _currentUser.profileImageDownloadURL = snapshot.value[@"profilePhotoDownloadURL"];
@@ -79,38 +122,12 @@
             [_usernameLabel setText:[NSString stringWithFormat:@"Hello, %@!", _currentUser.username]];
         });
     }];
-    
-}
-    -(void)presentCamera {
-        _imagePicker = [[UIImagePickerController alloc] init];
-        [_imagePicker setDelegate:self];
-        [_imagePicker setSourceType:UIImagePickerControllerSourceTypeCamera];
-        [self presentViewController:_imagePicker animated:true completion:nil];
 }
 
--(void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info {
-    NSData *imageData = UIImageJPEGRepresentation([info objectForKey:@"UIImagePickerControllerOriginalImage"], 1);
-    UIImage *image = [UIImage imageWithData:imageData];
-    NSData *resizedImgData =  UIImageJPEGRepresentation([self reduceImageSize:image], .50);
-    [self uploadPhotoToFirebase:resizedImgData];
-    [self dismissViewControllerAnimated:true completion:nil];
-}
-
--(UIImage *)reduceImageSize:(UIImage *)image {
-    CGSize newSize = CGSizeMake(image.size.width/6, image.size.height/6);
-    UIGraphicsBeginImageContextWithOptions(newSize, false, 0.0);
-    [image drawInRect:CGRectMake(0, 0, newSize.width, newSize.height)];
-    UIImage *smallImage = UIGraphicsGetImageFromCurrentImageContext();
-    return smallImage;
-}
-
-#pragma mark Firebase Methods
-
--(void)firebaseSetUp {
-    _firebaseStorage = [FIRStorage storage];
-    _firebaseStorageRef = [_firebaseStorage referenceForURL:@"gs://wire-e0cde.appspot.com"];
-}
-
+/*
+ This accepts the NSData that is returned from the image picker and then saves it on Firebase Storage.
+ Once it is stored in Firebase storage it gives us back a downloadURL.
+ */
 -(void)uploadPhotoToFirebase:(NSData *)imageData {
     //Create a uniqueID for the image and add it to the end of the images reference.
     NSString *uniqueID = [[NSUUID UUID]UUIDString];
@@ -129,28 +146,42 @@
     [uploadTask resume];
 }
 
+/*
+ This accepts a UserProfile object to update (which will be our current user profile).
+ When the UserProfile object is passed it will already have an updated URL from when the new photo
+ is taken and the metaDataURL is sent back. It then updates the child node on Firebase.
+ */
 -(void)updateCurrentUserProfileImageDownloadURLOnFirebaseDatabase:(UserProfile *)userProfile {
     
     FIRDatabaseReference *firebaseRef = [[FIRDatabase database] reference];
     
-    //Need every value filled or it will just remove what we didn't put in the dictionary. For an example if the profileImageDownloadURL was the only thing we put in this dictionary and used this dictionary to update the child node then the email, userId and the username would be removed and only the profilePhotoDownloadURL would be in that child node.
+    /*
+     Need every value filled or it will just remove what we didn't put in the dictionary.
+     For an example if the profileImageDownloadURL was the only thing we put in this dictionary
+     and used this dictionary to update the child node then the email, userId and the username
+     would be removed and only the profilePhotoDownloadURL would be in that child node.
+     */
     NSDictionary *userProfileToUpdate = @{@"profilePhotoDownloadURL": userProfile.profileImageDownloadURL,
                                           @"email": userProfile.email,
                                           @"userId": userProfile.uid,
                                           @"username": userProfile.username};
     
     NSDictionary *childUpdates = @{[@"/userprofile/" stringByAppendingString:_currentUserProfileKey]: userProfileToUpdate};
-
+    
     [firebaseRef updateChildValues:childUpdates];
-
+    
 }
 
+#pragma mark PrepareForSegue
+
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    NSLog(@"DOWNLOAD URL PREPARE FOR SEGUE: %@", _currentUser.profileImageDownloadURL);
+    //Sets the currentUserProfile property on the destination view controller
     ChatViewController *destVC = [segue destinationViewController];
     destVC.currentUserProfile = _currentUser;
 }
 
+#pragma mark Custom View Set Up
+//Rounds the profile photo
 -(void)viewWillLayoutSubviews {
     _currentUserProfilePhoto.layer.borderWidth = 4.0;
     _currentUserProfilePhoto.layer.borderColor = [[UIColor blackColor] CGColor];
@@ -158,6 +189,17 @@
     _currentUserProfilePhoto.layer.masksToBounds = TRUE;
 }
 
+#pragma mark IBActions
+
+- (IBAction)signOutBtnPressed:(id)sender {
+    NSError *error;
+    [[FIRAuth auth] signOut:&error];
+    if (!error) {
+
+    }
+}
+
+//Presents the camera when the profile photo is selected
 - (IBAction)profilePhotoSelected:(id)sender {
     [self presentCamera];
     
